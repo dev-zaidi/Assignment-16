8b89afe927eb2d08b5287530208791e8dd2cf3a2
dev-zaidi <syedahibaareej@gmail.com>
2024-09-10 21:24:06 +0500

    Assignment 16

---
+++ app.js
@@ -0,0 +1,90 @@
+// chap 6
+var x=1;
+x++;
+console.log(x);
+
+
+var xx=100;
+xx--;
+console.log(xx);
+
+
+var xxx=50,y=xxx++;
+console.log(y);
+
+var yy=50,z=--yy;
+console.log(z);
+
+var a=10;
+var newNum=a--;
+
+
+var catNew=a++;
+
+var cat=10;
+//alert(cat++);
+
+// chap 7
+var calculatedNum= 2+(2*6);
+console.log(calculatedNum);
+
+
+var calculatedNum=(2+2)*6;
+console.log(calculatedNum);
+
+
+var calculatedNum=(2+2)*(4+2);
+console.log(calculatedNum);
+
+
+var calculatedNum=((2+2)*4)/2;
+console.log(calculatedNum);
+
+
+var answerFive= (2+2)*(4+10);
+console.log(answerFive);
+
+var answerSix=2+(2*4)+10;
+console.log(answerSix);
+
+var answerSeven=4/(2*4);
+console.log(answerSeven);
+
+
+// chap 8
+var num="2"+"2";
+console.log(num);
+// anaswer will be 22 but as string dataType
+
+var message="Hello"+ "Dolly";
+// alert(message);
+
+// alert("33"+3);
+// alert box is displaying 333.
+
+// alert("Pakistan"+" Zaidabad");
+
+// alert ("23"+5);
+// alert displays 235
+
+var s="Sea";
+var h="Horse";
+var animal= s+" "+h;
+console.log(animal);
+
+
+// chap 9
+// var firstName=prompt("Enter First Name");
+
+// var country= prompt("Country?","China");
+
+// var yourName = prompt("Enter your Name");
+
+// var message= prompt("Message","This is a string");
+
+var first="one";
+var second="two";
+// var third= prompt(first,second);
+
+var questionSix= prompt("this is the last question","this is default text");
+alert(questionSix);
